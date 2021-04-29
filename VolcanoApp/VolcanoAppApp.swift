798f315a3f886698fe63bafeeef01776f28be8b5
kristbjörg <kristbjorg_@hotmail.com>
2021-04-29 19:24:47 +0000

    Initial Commit

---
+++ VolcanoApp/VolcanoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  VolcanoAppApp.swift
+//  VolcanoApp
+//
+//  Created by Kristbjorg Oskarsdottir on 29.4.2021.
+//
+
+import SwiftUI
+
+@main
+struct VolcanoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
